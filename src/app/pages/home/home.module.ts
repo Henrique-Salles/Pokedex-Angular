0028f31fd02e57009656924b791346752c6a9650
Henrique-Salles <henriquesalles1520@gmail.com>
2023-07-05 12:02:28 -0300

    header component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent],
   imports: [CommonModule],
   exports: [HomeComponent],
 })
