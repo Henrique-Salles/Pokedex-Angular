231caeb79c986bbd611ff8fd1a56fa7ab4535697
Henrique-Salles <henriquesalles1520@gmail.com>
2023-07-06 13:53:19 -0300

    details info

---
+++ src/app/pages/details/details-info/details-info.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-info',
+  templateUrl: './details-info.component.html',
+  styleUrls: ['./details-info.component.scss'],
+})
+export class DetailsInfoComponent {}
--- src/app/pages/details/details.module.ts
+++ src/app/pages/details/details.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './details.component';
 import { DetailsHeaderComponent } from './details-header/details-header.component';
 import { DetailsTypesComponent } from './details-types/details-types.component';
+import { DetailsInfoComponent } from './details-info/details-info.component';
 
 @NgModule({
-  declarations: [DetailsComponent, DetailsHeaderComponent, DetailsTypesComponent],
+  declarations: [DetailsComponent, DetailsHeaderComponent, DetailsTypesComponent, DetailsInfoComponent],
   imports: [CommonModule],
   exports: [DetailsComponent],
 })
