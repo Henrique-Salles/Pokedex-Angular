e77032e70a684af430351dcbe790e8a8f3dcf535
Henrique-Salles <henriquesalles1520@gmail.com>
2023-07-07 17:00:02 -0300

    details types BEM

--- src/app/pages/details/details-types/details-types.component.ts
+++ src/app/pages/details/details-types/details-types.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-details-types',
+  selector: 'poke-types',
   templateUrl: './details-types.component.html',
-  styleUrls: ['./details-types.component.scss']
+  styleUrls: ['./details-types.component.scss'],
 })
-export class DetailsTypesComponent {
-
-}
+export class DetailsTypesComponent {}
