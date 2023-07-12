e277801d143182fe43968dc984c8822e4236247e
Henrique-Salles <henriquesalles1520@gmail.com>
2023-07-12 15:08:19 -0300

    routing cards to details

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,10 +6,16 @@ import { HomeComponent } from './home.component';
 import { HeaderComponent } from './header/header.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 import { ListItemComponent } from './list-item/list-item/list-item.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent, PokeListComponent, ListItemComponent],
-  imports: [CommonModule],
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    PokeListComponent,
+    ListItemComponent,
+  ],
+  imports: [CommonModule, RouterModule],
   exports: [HomeComponent, PokeListComponent],
 })
 export class HomeModule {}
