3dc4bd31f9bdab9812b8afa391142769a7f25c85
Henrique-Salles <henriquesalles1520@gmail.com>
2023-07-18 14:35:13 -0300

    form adjusts

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
 import { ListItemComponent } from './list-item/list-item/list-item.component';
 import { RouterModule } from '@angular/router';
-import { PokeSearchComponent } from './header/search/poke-search/poke-search.component';
+import { PokeSearchComponent } from './poke-list/poke-search/poke-search.component';
 
 @NgModule({
   declarations: [
